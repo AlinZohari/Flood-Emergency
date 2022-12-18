3534afec08e36d9e6837368527f8fc982e7f67fe
Girish <ucesjoi@ucl.ac.uk>
2022-12-18 15:44:28 +0000

    Added Buffer Zone - Rish

--- main.py
+++ main.py
@@ -15,11 +15,15 @@ def user_input():
     x = float(input('Insert x coordinate:'))
     y = float(input('Insert y coordinate:'))
     input_point = Point([x, y])
+    buffer_zone = input_point.buffer(5000)          # Make a new function for this buffer task
+    xp, yp = buffer_zone.exterior.xy
     if 430000 <= x <= 465000 and 80000 <= y <= 95000:
+        plt.fill(xp, yp, 'bo')
         plt.plot(x, y, 'ro')
         return input_point
     else:
         print("The co-ordinates are out of bounds")
+        exit()
 
 
 if __name__ == "__main__":
