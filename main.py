df4a1f0939bc07c7c41154ffc8788d5530d215da
Girish <ucesjoi@ucl.ac.uk>
2022-12-18 15:30:15 +0000

    Rough version of Task 1 - Rish

--- main.py
+++ main.py
@@ -4,9 +4,28 @@ import pandas as pd
 import rasterio
 from rasterio import plot
 from shapely.geometry import Point, Polygon
+import matplotlib.pyplot as plt
+
+
+def user_input():
+    """
+    Takes user input and makes sure it's in the MBR
+    :return: returns a Point if it is inside the MBR
+    """
+    x = float(input('Insert x coordinate:'))
+    y = float(input('Insert y coordinate:'))
+    input_point = Point([x, y])
+    if 430000 <= x <= 465000 and 80000 <= y <= 95000:
+        plt.plot(x, y, 'ro')
+        return input_point
+    else:
+        print("The co-ordinates are out of bounds")
+
 
 if __name__ == "__main__":
     map = rasterio.open('D:/UCL/Geospatial Programming/Material/background/raster-50k_2724246.tif')
     dem = rasterio.open('D:/UCL/Geospatial Programming/Material/elevation/sz.asc')
-    # rasterio.plot.show(map)
+    user_input()
+    rasterio.plot.show(map)
+    plt.show()
 
