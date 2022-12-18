4624f4c7ad416782b23cab3a2d2f2afa5ed90b4c
Girish <ucesjoi@ucl.ac.uk>
2022-12-18 18:19:46 +0000

    Rough Code with Task 2 - Rish

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import numpy
 import geopandas as gpd
 import pandas as pd
 import rasterio
+import rasterio.mask
+import rasterio.windows
 from rasterio import plot
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Point, LineString, Polygon
 import matplotlib.pyplot as plt
 
 
@@ -15,10 +17,8 @@ def user_input():
     x = float(input('Insert x coordinate:'))
     y = float(input('Insert y coordinate:'))
     input_point = Point([x, y])
-    buffer_zone = input_point.buffer(5000)          # Make a new function for this buffer task
-    xp, yp = buffer_zone.exterior.xy
+
     if 430000 <= x <= 465000 and 80000 <= y <= 95000:
-        plt.fill(xp, yp, 'bo')
         plt.plot(x, y, 'ro')
         return input_point
     else:
@@ -26,10 +26,37 @@ def user_input():
         exit()
 
 
+def get_buffer(point):
+    buffer_zone = point.buffer(5000)  # Make a new function for this buffer task
+    xp, yp = buffer_zone.exterior.xy
+    plt.fill(xp, yp, 'bo')
+    return buffer_zone
+
+
 if __name__ == "__main__":
     map = rasterio.open('D:/UCL/Geospatial Programming/Material/background/raster-50k_2724246.tif')
     dem = rasterio.open('D:/UCL/Geospatial Programming/Material/elevation/sz.asc')
-    user_input()
+
+    user_point = user_input()
+    study_buffer = get_buffer(user_point)
+    print(study_buffer)
+    # study_buffer = study_buffer.to_crs({'init': 'epsg:27700'})
+    study_image, study_transform = rasterio.mask.mask(dem, [study_buffer], crop=True, filled=True)
+    print(rasterio.mask.mask(dem, [study_buffer], crop=True, filled=True))
+    """
+        Masked Method: Not Successful
+    study_meta = dem.meta
+    study_meta.update({"driver": "GTiff",
+                      "height": study_image.shape[1],
+                       "width:": study_image.shape[2],
+                       "transform": study_transform})
+
+    # rasterio.plot.show(dem)
+    with open("D:/UCL/Geospatial Programming/RGB.byte.masked.tif", 'w', **study_meta) as dest:
+        dest.write(study_image)
+    """
+    rasterio.plot.show(study_image, transform=study_transform)
     rasterio.plot.show(map)
+
     plt.show()
 
