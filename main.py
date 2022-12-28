1d1e90da3995d598ab88ec00f0f9acad60c48406
Girish <ucesjoi@ucl.ac.uk>
2022-12-28 19:55:39 +0000

    Found the closest path (Rough Code) - Rish

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ import rasterio.mask
 import rasterio.windows
 from rasterio import features
 from rasterio import plot
-from shapely.geometry import Point, LineString, Polygon
+from shapely.geometry import Point, LineString, Polygon, box
 import matplotlib.pyplot as plt
+import os
+import json
+import networkx as nx
+import scipy as sp
 
 
 def user_input():
@@ -65,31 +69,96 @@ if __name__ == "__main__":
 
     user_point = user_input()
     study_buffer = get_buffer(user_point)
+    rasterio.plot.show(main_map, extent=study_buffer)
+    print(study_buffer)
 
     # Task 2: Returns the point of highest elevation
     highest_elev = get_highest_point(study_buffer)
     print(highest_elev)
 
-    # study_image = rasterio.mask.mask(dem, [study_buffer], crop=True, filled=True)    #Main Study Area
-    # print(study_image)
-    # print(study_buffer.bounds[0], study_buffer.bounds[3])
+    # Task 3: Working with ITN
+    # itn_network = pd.read_json('D:/UCL/Geospatial Programming/Material/itn/solent_itn.json')
+    # print(itn_network.head())
+    itn_json_path = os.path.join('D:/UCL/Geospatial Programming/Material/itn/solent_itn.json')
+    with open(itn_json_path, 'r') as f:
+        itn_json = json.load(f)
+
+    itn_nodes = nx.Graph()
+    road_links = itn_json['roadlinks']
+    road_nodes = itn_json['roadnodes']
+    road_links_inside_buffer = {}
+    road_nodes_inside_buffer = {}
+    all_new_line_strings = []
+    all_new_points = []
+
+    for links in road_links:
+        new_line_string = LineString(road_links[links]['coords'])
+        if study_buffer.contains(new_line_string):
+            road_links_inside_buffer[links] = road_links[links]
+            all_new_line_strings.append(new_line_string)
+
+    for nodes in road_nodes:
+        new_node_point = Point(road_nodes[nodes]['coords'])
+        if study_buffer.contains(new_node_point):
+            road_nodes_inside_buffer[nodes] = road_nodes[nodes]
+            all_new_points.append(new_node_point)
+
+    closest_distance_user_point_dist = 100000
+    closest_distance_highest_elev_dist = 100000
+    closest_distance_user_point = Point()
+    closest_distance_highest_elev = Point()
+
+    for x in all_new_points:
+        if user_point.distance(x) < closest_distance_user_point_dist:
+            closest_distance_user_point = x
+            closest_distance_user_point_dist = user_point.distance(x)
+        else:
+            pass
+
+    for x in all_new_points:
+        if highest_elev.distance(x) < closest_distance_highest_elev_dist:
+            closest_distance_highest_elev = x
+            closest_distance_highest_elev_dist = highest_elev.distance(x)
+
+    print(closest_distance_user_point)
+    print(closest_distance_highest_elev)         # check the for loop for error
+
+    closest_node_id_user = ''
+    closest_node_id_high_elev = ''
+
+    for nodes in road_nodes_inside_buffer:
+        if [closest_distance_user_point.x, closest_distance_user_point.y] == road_nodes_inside_buffer[nodes]['coords']:
+            closest_node_id_user = nodes
+
+    for nodes in road_nodes_inside_buffer:
+        if [closest_distance_highest_elev.x, closest_distance_highest_elev.y] == road_nodes_inside_buffer[nodes]['coords']:
+            closest_node_id_high_elev = nodes
+
+    print(closest_node_id_user)
+    print(closest_node_id_high_elev)
+
+
+
+    # print(road_links_inside_buffer)
+    for link in road_links_inside_buffer:
+        itn_nodes.add_edge(road_links_inside_buffer[link]['start'], road_links_inside_buffer[link]['end'], fid = link, weight = road_links_inside_buffer[link]['length'])
+
+    path = nx.dijkstra_path(itn_nodes, source=closest_node_id_user, target=closest_node_id_high_elev, weight='weight')
+    print(path)
+    print(len(road_links_inside_buffer))
+    print(len(road_links))
+
+    # fig, ax = plt.subplots()
+    # for line in all_new_line_strings:
+    #     x, y = line.xy
+    #     ax.plot(x, y)
+    # print(len(all_new_line_strings))
     #
-    # indices = numpy.where(study_image[0] == study_image[0].max())
-    # print(indices)
-    # print(indices[2][0])
-    # print(indices[1][0])
-    # print(study_image[1].bounds)
-    #
-    # x = study_buffer.bounds[0] + indices[2][0] * 5
-    # y = study_buffer.bounds[3] - indices[1][0] * 5
-    #
-    # print(x, y)
-    #
-    # max_elev = numpy.max(study_image[0])
-    # print(max_elev)
-    # plt.plot(x,y, 'ro')
-    #
-    # rasterio.plot.show(study_image[0], transform=study_image[1])
-    rasterio.plot.show(main_map, extent=study_buffer)              # overlap the buffer and the map together
-                                                                   # It's not working now but it's intentional
+    # for point in all_new_points:
+    #     x, y = point.xy
+    #     ax.plot(x, y, 'ro')
+
+    # nx.draw(itn_nodes, node_size=1)
+
+
     plt.show()
